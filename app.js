1f3fa053c08b139db6b536d18c1615e06969d8ba
Diego Mosan <diego14290764@itcg.edu.mx>
2022-03-10 14:06:31 -0600

    Adding validation for special attac

--- app.js
+++ app.js
@@ -3,20 +3,27 @@ const app = Vue.createApp({
   data(){
     return {
       playerHealth: 100,
-      monsterHealth: 100
+      monsterHealth: 100,
+      rounds: 0
     }
   },
   methods:{
     attackMonster(){
+      this.rounds++;
      const attackValue =  getRandomValue(12,5)
      this.monsterHealth -= attackValue;
-     console.log(this.monsterHealth)
      this.attackPlayer();
     },
     attackPlayer(){
       const attackValue = getRandomValue(15,8)
       this.playerHealth -= attackValue;
-     }
+    },
+    specialAttackMonster(){
+      this.rounds++;
+      const attackValue = getRandomValue(25,10)
+      this.monsterHealth -= attackValue;
+     this.attackPlayer();
+    }
   }, 
   computed: {
     monsterBarStyles(){
@@ -24,6 +31,9 @@ const app = Vue.createApp({
     },
     playerBarStyles(){
       return {width: this.playerHealth +'%' }
+    },
+    mayUseSpecialAttack(){
+      return this.round % 3 !== 0
     }
   }
 
