5b666654570cff53d2034e2db264f8ce385a13f2
Diego Mosan <diego14290764@itcg.edu.mx>
2022-03-10 19:05:02 -0600

    Adding surrender logic and methods for if

--- app.js
+++ app.js
@@ -1,53 +1,85 @@
-const getRandomValue = (max, min) =>Math.round(Math.random() * (max-min)) + min;
+const getRandomValue = (max, min) =>
+  Math.round(Math.random() * (max - min)) + min;
 const app = Vue.createApp({
-  data(){
+  data() {
     return {
       playerHealth: 100,
       monsterHealth: 100,
-      rounds: 0
-    }
+      rounds: 0,
+      winner: null,
+    };
   },
-  methods:{
-    attackMonster(){
+  methods: {
+    attackMonster() {
       this.rounds++;
-     const attackValue =  getRandomValue(12,5)
-     this.monsterHealth -= attackValue;
-     this.attackPlayer();
+      const attackValue = getRandomValue(12, 5);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
     },
-    attackPlayer(){
+    attackPlayer() {
       this.rounds++;
-      const attackValue = getRandomValue(15,8)
+      const attackValue = getRandomValue(15, 8);
       this.playerHealth -= attackValue;
     },
-    specialAttackMonster(){
+    specialAttackMonster() {
       this.rounds++;
-      const attackValue = getRandomValue(25,10)
+      const attackValue = getRandomValue(25, 10);
       this.monsterHealth -= attackValue;
-     this.attackPlayer();
+      this.attackPlayer();
     },
-    healPlayer(){
+    healPlayer() {
       this.rounds++;
       const heal = getRandomValue(20, 8);
-      if(this.playerHealth + heal > 100){
+      if (this.playerHealth + heal > 100) {
         this.playerHealth = 100;
       } else {
-        this.playerHealth += heal
+        this.playerHealth += heal;
       }
-      this.attackPlayer()
+      this.attackPlayer();
+    },
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.rounds = 0;
+      this.winner = null;
+    },
+    surrender(){
+      this.winner = 'monster'
     }
-  }, 
+  },
   computed: {
-    monsterBarStyles(){
-      return {width: this.monsterHealth +'%' }
+    monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      }
+      return { width: this.monsterHealth + "%" };
     },
-    playerBarStyles(){
-      return {width: this.playerHealth +'%' }
+    playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: "0%" };
+      }
+      return { width: this.playerHealth + "%" };
     },
-    mayUseSpecialAttack(){
-      return this.rounds % 3 !== 0
-    }
-  }
+    mayUseSpecialAttack() {
+      return this.rounds % 3 !== 0;
+    },
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winner = "monster";
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winner = "player";
+      }
+    },
+  },
+});
 
-})
-
-app.mount('#game')
+app.mount("#game");
